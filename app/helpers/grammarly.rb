7df4235fc7d951372502a29a64412061fe402b58
ayva <olga.ayva@gmail.com>
2015-10-02 14:25:09 -0700

    checking grammar

--- app/helpers/grammarly.rb
+++ app/helpers/grammarly.rb
@@ -1,15 +1,12 @@
 module GrammarCheck
+  
 
   def check_errors(text)
-    api_url = Rails.application.secrets.grammarly_API_url
-
-    headers = {"Connect-Type" => "text/plain",
-                "Accept" => "application/json"}
-
-    response = HTTParty.post(api_url, :body => text, :headers =>headers)
-
-    data = response.parsed_response
-
-    puts data
+      
+      api_url = Rails.application.secrets.grammarly_API_url
+      headers = {"Content-Type" => "text/plain",
+                  "Accept" => "application/json"}
+      return HTTParty.post(api_url, :headers => headers, :body => text).parsed_response
+    
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'grammarly'
+text="Check my grammar"
+
+GrammarCheck::check_errors(text)
+

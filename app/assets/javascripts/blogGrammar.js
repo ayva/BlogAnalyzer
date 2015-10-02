d2e1215940338aec467de1b5173712dfe6549f34
David <davidmeza@outlook.com>
2015-10-02 13:29:13 -0400

    add angular imports

---
+++ app/assets/javascripts/blogGrammar.js
@@ -0,0 +1,36 @@
+bg = angular.module('bg', ['restangular', 'ui.router', 'ui.bootstrap'])
+
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+
+      .state('app', {
+        url: '/',
+        views: {
+          'navbar': {
+            templateUrl: 'templates/partials/navbar.html'
+          },
+
+          'content': {
+            templateUrl: 'templates/partials/content.html'
+          },
+
+          'footer': {
+            templateUrl: 'templates/partials/footer.html'
+          },
+
+        },
+      })
+
+
+
+    $urlRouterProvider.otherwise('/');
+
+  })
+
+
+  // enable error handling
+  .run(function($rootScope){
+    $rootScope.$on("$stateChangeError", console.log.bind(console));
+  });

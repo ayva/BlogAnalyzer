16ea72da65a0761786b741929955da9c67865331
David <davidmeza@outlook.com>
2015-10-09 19:06:00 -0400

    typeahead working

--- app/assets/javascripts/controllers/navbarCtrl.js
+++ app/assets/javascripts/controllers/navbarCtrl.js
@@ -4,16 +4,9 @@ grandma.controller('navbarCtrl', ['$scope', '$location', '$state', 'storage',
   if ($location.path() === '/')
     $scope.landing = true;
 
-  $scope.users = [
-    {username: "annaharari", id: 1},
-    {username: "gavinsheridan", id: 2}
-  ];
-
-
-  // $scope.users = storage.bloggers.all;
+  $scope.users = storage.bloggers;
 
   $scope.go = function(user) {
-    console.log(user);
     $state.go("blogger", {username: user.username, id: user.id })
   }
 

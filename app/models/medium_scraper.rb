82ae24d2c17a866ea4518c4060298e223bda0c18
shadefinale <shadefinale@gmail.com>
2015-10-15 15:35:16 -0500

    Make scraper always capitalize name.

--- app/models/medium_scraper.rb
+++ app/models/medium_scraper.rb
@@ -167,7 +167,7 @@ class MediumScraper
     page = agent.get(author_url)
 
     url = author_url
-    name = page.search('//*[@id="prerendered"]/div[2]/div/header/h1').text
+    name = page.search('//*[@id="prerendered"]/div[2]/div/header/h1').text.split(" ").map(&:capitalize).join(" ")
     username = MediumScraper.add_username(name)
     img = page.search('//*[@id="prerendered"]/div[2]/div/header/div[1]/div[2]/img')[0].attributes["src"].text
     twtr = MediumScraper.get_twitter(page)
@@ -183,7 +183,7 @@ class MediumScraper
                                             twitter: twtr,
                                             facebook: fcbk,
                                             author_pic: img)
-      # TwitterAPI.new.delay.twit(Author.last.full_name, Author.last.id)
+      TwitterAPI.new.delay.twit(Author.last.full_name, Author.last.id)
       return new_author
     end
   end
